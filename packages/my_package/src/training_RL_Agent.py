bb34cebd28084f8ee1a4c778bbf7ba1fd771268e
Deep Learning Lab CSU East Bay <130392310+deeplearning-lab-csueb@users.noreply.github.com>
2025-07-01 11:20:37 -0700

    Update training_RL_Agent.py

--- packages/my_package/src/training_RL_Agent.py
+++ packages/my_package/src/training_RL_Agent.py
@@ -1,5 +1,8 @@
 import os
 os.environ["PYGLET_HEADLESS"] = "1"
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(1024, 768))
+display.start()
 
 import gym
 import gym_duckietown
